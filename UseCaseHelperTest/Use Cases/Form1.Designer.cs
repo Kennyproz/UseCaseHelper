d60924483e12729af2ccda23c6510619f639b167
Ken <k.odigwegomez@student.fontys.nl>
2016-09-02 11:58:41 +0200

    Begin 3

---
+++ UseCaseHelperTest/Use Cases/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Use_Cases
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbActor = new System.Windows.Forms.RadioButton();
+            this.rbUseCase = new System.Windows.Forms.RadioButton();
+            this.rbLine = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pbTeken = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rbAdd = new System.Windows.Forms.RadioButton();
+            this.rbRemove = new System.Windows.Forms.RadioButton();
+            this.btClear = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTeken)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rbActor
+            // 
+            this.rbActor.AutoSize = true;
+            this.rbActor.Location = new System.Drawing.Point(38, 43);
+            this.rbActor.Name = "rbActor";
+            this.rbActor.Size = new System.Drawing.Size(50, 17);
+            this.rbActor.TabIndex = 0;
+            this.rbActor.TabStop = true;
+            this.rbActor.Text = "Actor";
+            this.rbActor.UseVisualStyleBackColor = true;
+            // 
+            // rbUseCase
+            // 
+            this.rbUseCase.AutoSize = true;
+            this.rbUseCase.Location = new System.Drawing.Point(9, 45);
+            this.rbUseCase.Name = "rbUseCase";
+            this.rbUseCase.Size = new System.Drawing.Size(70, 17);
+            this.rbUseCase.TabIndex = 1;
+            this.rbUseCase.TabStop = true;
+            this.rbUseCase.Text = "Use case";
+            this.rbUseCase.UseVisualStyleBackColor = true;
+            // 
+            // rbLine
+            // 
+            this.rbLine.AutoSize = true;
+            this.rbLine.Location = new System.Drawing.Point(38, 91);
+            this.rbLine.Name = "rbLine";
+            this.rbLine.Size = new System.Drawing.Size(45, 17);
+            this.rbLine.TabIndex = 2;
+            this.rbLine.TabStop = true;
+            this.rbLine.Text = "Line";
+            this.rbLine.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbUseCase);
+            this.groupBox1.Location = new System.Drawing.Point(29, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(116, 99);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Elementen";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // pbTeken
+            // 
+            this.pbTeken.BackColor = System.Drawing.Color.White;
+            this.pbTeken.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbTeken.Location = new System.Drawing.Point(29, 126);
+            this.pbTeken.Name = "pbTeken";
+            this.pbTeken.Size = new System.Drawing.Size(483, 259);
+            this.pbTeken.TabIndex = 4;
+            this.pbTeken.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rbRemove);
+            this.groupBox2.Controls.Add(this.rbAdd);
+            this.groupBox2.Location = new System.Drawing.Point(170, 21);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(116, 99);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Modus";
+            // 
+            // rbAdd
+            // 
+            this.rbAdd.AutoSize = true;
+            this.rbAdd.Location = new System.Drawing.Point(3, 16);
+            this.rbAdd.Name = "rbAdd";
+            this.rbAdd.Size = new System.Drawing.Size(44, 17);
+            this.rbAdd.TabIndex = 0;
+            this.rbAdd.TabStop = true;
+            this.rbAdd.Text = "Add";
+            this.rbAdd.UseVisualStyleBackColor = true;
+            // 
+            // rbRemove
+            // 
+            this.rbRemove.AutoSize = true;
+            this.rbRemove.Location = new System.Drawing.Point(3, 39);
+            this.rbRemove.Name = "rbRemove";
+            this.rbRemove.Size = new System.Drawing.Size(65, 17);
+            this.rbRemove.TabIndex = 1;
+            this.rbRemove.TabStop = true;
+            this.rbRemove.Text = "Remove";
+            this.rbRemove.UseVisualStyleBackColor = true;
+            // 
+            // btClear
+            // 
+            this.btClear.Location = new System.Drawing.Point(437, 21);
+            this.btClear.Name = "btClear";
+            this.btClear.Size = new System.Drawing.Size(75, 23);
+            this.btClear.TabIndex = 6;
+            this.btClear.Text = "Clear all";
+            this.btClear.UseVisualStyleBackColor = true;
+            this.btClear.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 397);
+            this.Controls.Add(this.btClear);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.pbTeken);
+            this.Controls.Add(this.rbLine);
+            this.Controls.Add(this.rbActor);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "User Case Helper Ken";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTeken)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbActor;
+        private System.Windows.Forms.RadioButton rbUseCase;
+        private System.Windows.Forms.RadioButton rbLine;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pbTeken;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rbRemove;
+        private System.Windows.Forms.RadioButton rbAdd;
+        private System.Windows.Forms.Button btClear;
+    }
+}
+
